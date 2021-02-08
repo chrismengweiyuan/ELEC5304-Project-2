2ee511a1c8284bd9ae97b4a98b168cea2b8b2942
chrismengweiyuan <78723324+chrismengweiyuan@users.noreply.github.com>
2021-02-08 16:47:37 +0800

    Add files via upload

---
+++ model.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+import torch.nn.init as init
+
+
+class DnCNN(nn.Module):
+    def __init__(self, depth=17, n_channels=64, image_channels=1, use_bnorm=True, kernel_size=3):
+        super(DnCNN, self).__init__()
+        kernel_size = 3
+        padding = 1
+        layers = []
+
+        layers.append(nn.Conv2d(in_channels=image_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding, bias=True))
+        layers.append(nn.ReLU(inplace=True))
+        for _ in range(depth-3):
+            layers.append(nn.Conv2d(in_channels=n_channels, out_channels=n_channels, kernel_size=kernel_size, padding=padding, bias=False))
+            layers.append(nn.BatchNorm2d(n_channels, eps=0.0001, momentum = 0.95))
+            layers.append(nn.ReLU(inplace=True))
+        
+        # a layer is added for smoothing the output. the number of output channels is mapped from 64 to 32 and 1 at output
+        layers.append(nn.Conv2d(in_channels=n_channels, out_channels=round(n_channels/2), kernel_size=kernel_size, padding=padding, bias=False))
+        layers.append(nn.BatchNorm2d(round(n_channels/2), eps=0.0001, momentum = 0.95))
+        layers.append(nn.ReLU(inplace=True))
+            
+        layers.append(nn.Conv2d(in_channels=round(n_channels/2), out_channels=image_channels, kernel_size=kernel_size, padding=padding, bias=False))
+        self.dncnn = nn.Sequential(*layers)
+        self._initialize_weights()
+
+    def forward(self, x):
+        y = x
+        out = self.dncnn(x)
+        return y-out
+
+    def _initialize_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                init.orthogonal_(m.weight)
+                print('init weight')
+                if m.bias is not None:
+                    init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant_(m.weight, 1)
+                init.constant_(m.bias, 0)
